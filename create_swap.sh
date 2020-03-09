99cdc0ea628e58114674beba3f6b266d493c12ab
Alexander Popiak <alexander.popiak@parity.io>
2020-03-09 10:59:02 +0100

    add create_swap

---
+++ create_swap.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -ex
+
+fpath=$1
+size=$2
+
+# create swapfile
+sudo dd if=/dev/zero of=$fpath bs=1024 count=$size
+
+# change ownership to root and make it non-readable to other users
+sudo chown root:root $fpath
+sudo chmod 0600 $fpath
+
+# register swapfile with os
+sudo mkswap $fpath
+
+# and turn it on
+sudo swapon $fpath
+
+# register the swapfile with the os for startup
+echo "$fpath	swap	swap	sw	0	0" | sudo tee -a /etc/fstab
+
+# print out swaps
+cat /proc/swaps
